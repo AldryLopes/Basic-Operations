0002d42cc79c1ae7f3e50b106ba377ac9a2f6861
AldryLopes <aldrylopes@gmail.com>
2023-03-09 16:03:54 -0300

    Projeto inicial - Operações básicas

---
+++ math_operations.js
@@ -0,0 +1,16 @@
+let numberOne = prompt('Digite o primeiro número')
+let numberTwo = prompt('Digite o segundo número')
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+
+let sum = (numberOne + numberTwo)
+let sub = (numberOne - numberTwo)
+let mult = (numberOne * numberTwo)
+let div = (numberOne / numberTwo)
+let restDiv = (numberOne % numberTwo)
+
+alert(`A soma dos dois números é: ${sum}`)
+alert(`A subtração dos dois números é: ${sub}`)
+alert(`A multiplicação dos dois números é: ${mult}`)
+alert(`A divisão dos dois números é: ${div.toFixed(1)}`)
+alert(`A resto da divisão dos dois números é: ${restDiv}`)
