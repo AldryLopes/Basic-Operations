aa0f7208856fa5d42655e6d0bb1a327a4f0857a9
AldryLopes <aldrylopes@gmail.com>
2023-03-09 16:22:55 -0300

    Inserções de condicional e de novo arquivo

--- math_operations.js
+++ math_operations.js
@@ -8,9 +8,21 @@ let sub = (numberOne - numberTwo)
 let mult = (numberOne * numberTwo)
 let div = (numberOne / numberTwo)
 let restDiv = (numberOne % numberTwo)
+let restSum = sum
 
 alert(`A soma dos dois números é: ${sum}`)
 alert(`A subtração dos dois números é: ${sub}`)
 alert(`A multiplicação dos dois números é: ${mult}`)
 alert(`A divisão dos dois números é: ${div.toFixed(1)}`)
 alert(`A resto da divisão dos dois números é: ${restDiv}`)
+
+if(restSum % 2 == 0 && numberOne == numberTwo){
+  alert(`A soma dos dois numeros é par: ${sum}`)
+  alert(`Os números inseridos ${numberOne} e ${numberTwo} são iguais:`)
+}else if(restSum % 2 == 0 && numberOne !== numberTwo){
+  alert(`A soma dos dois numeros é par: ${sum}`)
+  alert(`Os números inseridos ${numberOne} e ${numberTwo} são diferentes:`)
+}else{
+  alert(`A soma dos dois numeros é ímpar: ${sum}`)
+  alert(`Os números inseridos ${numberOne} e ${numberTwo} são diferentes:`)
+}
